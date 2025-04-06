b8e0fea31e43e3c265b0c345140049590b16e9e2
5yndr0m <sdilanjana21@gmail.com>
2025-04-06 05:53:53 +0530

    feat: add name to navbar

---
+++ src/components/common/navbar.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <Link href="/" className="font-bold text-xl">Syndrom</Link>
+      </div>
+    </div>
+  );
+}

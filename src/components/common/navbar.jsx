179e6e0f5290cb1e8409bbc92fdf102c499d8a6b
5yndr0m <sdilanjana21@gmail.com>
2025-04-06 06:00:15 +0530

    feat: addning navigation menu items

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -2,13 +2,24 @@
 
 import Link from "next/link";
 import React from "react";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from "../ui/navigation-menu";
 
 export default function Navbar() {
   return (
-    <div>
-      <div>
-        <Link href="/" className="font-bold text-xl">Syndrom</Link>
+    <div className="container flex h-16 items-center justify-between">
+      <div className="flex items-center gap-2">
+        <Link href="/" className="font-bold text-xl">
+          Syndrom
+        </Link>
       </div>
+
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+          <NavigationMenuTrigger>About</NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 }

bd37d63919c4171bf0535bc804b24d4f48193f10
5yndr0m <sdilanjana21@gmail.com>
2025-04-06 20:57:50 +0530

    feat: add project data

---
+++ src/lib/projectData.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: "DisasterWatch",
+    description: "A Crowd Sourced Real-Time Multi-Disaster Notification and Alert App.",
+    image: "/disasterwatch.svg?height=100&width=100",
+    tags: ["Next.js", "Expo", "Node.js", "WebPush", "MongoDB"],
+    status: "In Progress",
+    github: "https://github.com/DisasterWatchHQ",
+    live: "https://disasterwatch.vercel.app/",
+  },
+  {
+    title: "Studius",
+    description: "Centralized task management that caters to students.",
+    image: "/placeholder.svg?height=300&width=600",
+    tags: ["React", "JavaScript"],
+    status: "Completed",
+    github: "https://github.com/5yndr0m/IT2342024studis",
+    live: null,
+  },
+  {
+    title: "CLI To-Do App",
+    description: "CLI based To-Do app made using python.",
+    image: "/placeholder.svg?height=300&width=600",
+    tags: ["Python",],
+    status: "Completed",
+    github: "https://github.com/5yndr0m/CLI-ToDoApp",
+    live: null,
+  },
+  {
+    title: "Portfolio Website",
+    description: "Personal portfolio website built with Next.js and Shadcn UI.",
+    image: "/placeholder.svg?height=300&width=600",
+    tags: ["Next.js", "Tailwind CSS", "Shadcn"],
+    status: "In Progress",
+    github: "https://github.com/5yndr0m/syndrom.me",
+    live: "https://alexchen.dev",
+  },
+]
